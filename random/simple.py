5cf60ac116f1d75f3d44a0520cd6bc295fff5281
Brian Matejek <bmatejek@gmail.com>
2020-10-07 13:19:43 -0400

    Added configuration model

--- random/simple.py
+++ random/simple.py
@@ -1,3 +1,4 @@
+import os
 import random
 
 
@@ -11,7 +12,7 @@ from addax.visualize.graph import VisualizeGraph
 def GenerateSimpleRandomGraphs(N, nnodes, nedges, directed):
     """
     Generate a simple random graph that randomly chooses edges to connect vertices
-    
+
     @param N: the number of random graphs to generate
     @param nnodes: the number of nodes in the random graph
     @param nedges: the number of edges in the random graph
@@ -32,6 +33,7 @@ def GenerateSimpleRandomGraphs(N, nnodes, nedges, directed):
         edges = edges[:nedges]
 
         # create a new graph object
+        prefix = 'nnodes-{}-nedges-{}-directed-{:05d}'.format(nnodes, nedges, ii)
         graph = Graph(directed)
 
         # add the vertex indices (there are no communities in this simple graph)
@@ -44,13 +46,67 @@ def GenerateSimpleRandomGraphs(N, nnodes, nedges, directed):
             graph.AddEdge(edge[0], edge[1])
 
         # write the simple graph to file
-        if directed: output_filename = 'random/simple/graphs/nnodes-{}-nedges-{}-directed-{:05d}.graph'.format(nnodes, nedges, ii)
-        else: output_filename = 'random/simple/graphs/nnodes-{}-nedges-{}-undirected-{:05d}.graph'.format(nnodes, nedges, ii)
+        if directed: output_filename = 'random/simple/graphs/{}.graph'.format(prefix)
+        else: output_filename = 'random/simple/graphs/{}.graph'.format(prefix)
 
         WriteGraph(graph, output_filename)
 
         # visualize the graph
-        if directed: output_filename = 'random/simple/visuals/nnodes-{}-nedges-{}-directed-{:05d}.dot'.format(nnodes, nedges, ii)
-        else: output_filename = 'random/simple/visuals/nnodes-{}-nedges-{}-undirected-{:05d}.dot'.format(nnodes, nedges, ii)
+        if directed: output_filename = 'random/simple/visuals/{}.dot'.format(prefix)
+        else: output_filename = 'random/simple/visuals/{}.dot'.format(prefix)
 
         VisualizeGraph(graph, output_filename)
+
+
+
+def ConfigurationModelRandomGraphs(graph, ngraphs):
+    """
+    Generate random graphs using a configuration model which guarantees the same degree distribution.
+    This simple method ignores all communities.
+
+    @param graph: the input graph to copy
+    @param ngraphs: the number of random graphs to create
+    """
+    for graph_index in range(ngraphs):
+        # create the degree lists for the in and out
+        incoming_degree_list = []
+        outgoing_degree_list = []
+
+        for edge in graph.edges:
+            incoming_degree_list.append(edge.source_index)
+            outgoing_degree_list.append(edge.destination_index)
+
+        # shuffle both the incoming and outgoing edge lists
+        random.shuffle(incoming_degree_list)
+        random.shuffle(outgoing_degree_list)
+
+        # get a new prefix for the random graph
+        prefix = '{}-{:06d}'.format(graph.prefix, graph_index)
+
+        # construct a random graph
+        random_graph = Graph(prefix, graph.directed)
+
+        # create vertices for the random graph
+        for vertex_index in graph.vertices.keys():
+            random_graph.AddVertex(vertex_index)
+
+        edges = set()
+
+        # add edges to the graph
+        nedges = len(incoming_degree_list)
+        for ie in range(nedges):
+            # skip self loops
+            if incoming_degree_list[ie] == outgoing_degree_list[ie]: continue
+            # skip multi edges
+            if (incoming_degree_list[ie], outgoing_degree_list[ie]) in edges: continue
+
+            random_graph.AddEdge(incoming_degree_list[ie], outgoing_degree_list[ie])
+
+            edges.add((incoming_degree_list[ie], outgoing_degree_list[ie]))
+
+        # write the graph to file
+        output_directory = 'random/simple-configuration-model/graphs'
+        if not os.path.exists(output_directory):
+            os.makedirs(output_directory, exist_ok = True)
+        output_filename = '{}/{}.graph'.format(output_directory, random_graph.prefix)
+        WriteGraph(random_graph, output_filename)
