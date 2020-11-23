e89fc0dee7f7204bddbfe736cbbe98224cedf6e0
Brian Matejek <bmatejek@gmail.com>
2020-11-23 14:51:40 -0500

    degree: created analysis for synaptic weights

--- analysis/degree.py
+++ analysis/degree.py
@@ -2,6 +2,9 @@ import math
 
 
 
+import numpy as np
+
+
 import matplotlib
 import matplotlib.pyplot as plt
 
@@ -56,3 +59,46 @@ def ModelDegreeDistribution(graph):
     PlotDegrees(degrees)
     PlotDegrees(incoming_degrees)
     PlotDegrees(outgoing_degrees)
+
+
+
+def PlotSynapseWeights(graph):
+    """
+    Plot the number of edges as a function of the synapse weights
+
+    @param graph: the graph for which to analyze the edge distribution
+    """
+    # create an array of edge weights
+    edge_weights = []
+    for edge in graph.edges:
+        edge_weights.append(int(round(edge.weight)))
+
+    # sort the edge weights with the lowest number first
+    edge_weights.sort()
+
+    # get the minimum and maximum edge weights
+    min_weight = edge_weights[0]
+    max_weight = edge_weights[-1]
+
+    # create an array for the number of edges greater than a given weight
+    edges_over_weight = [0 for _ in range(max_weight + 1)]
+
+    # iterate over the edge weights list
+    edge_weight_index = 0
+    for weight in range(max_weight + 1):
+        # continually increment the edge weight index to
+        # remove all edges less than this weight
+        while edge_weights[edge_weight_index] < weight:
+            edge_weight_index += 1
+
+        # get the number of edges with weights greater than this
+        edges_over_weight[weight] =len(edge_weights) - edge_weight_index
+
+    plt.plot([math.log(iv, 10) for iv in range(min_weight, max_weight + 1)], edges_over_weight[1:])
+
+    # plot figure titles/axes
+    plt.title('Edge Weights')
+    plt.xlabel('Synaptic Weight (Log Scale)')
+    plt.ylabel('Edges with More Weight')
+
+    plt.savefig('figures/{}/synaptic-weight.png'.format(graph.prefix.title().replace('-', '')))
