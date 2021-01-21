6e2a82a6581a3aca6dfdcd0e48161836c37594cd
Brian Matejek <bmatejek@gmail.com>
2021-01-21 14:22:59 -0500

    hemibrain: lowered threshold for connection based on bioarxiv

--- hemibrain/construction.py
+++ hemibrain/construction.py
@@ -5,11 +5,11 @@ import time
 
 
 from addax.data_structures.graph import Graph
-from addax.utilities.dataIO import ReadGraph, WriteGraph
+from addax.utilities.dataIO import ReadGraph, WriteGraph, PickleData
 
 
 
-def ConstructGraphFromHemiBrainCSV(LOW_EDGE_THRESHOLD = 5):
+def ConstructGraphFromHemiBrainCSV(LOW_EDGE_THRESHOLD = 4):
     """
     Construct a graph object for the hemi brain CSV files
 
@@ -19,7 +19,10 @@ def ConstructGraphFromHemiBrainCSV(LOW_EDGE_THRESHOLD = 5):
     start_time = time.time()
 
     neurons = set()
+    neuron_ids = set()
     edges = {}
+
+    types = set()
 
     # open the neuron csv file
     neuron_filename = 'CSVs/HemiBrain/traced-neurons.csv'
@@ -30,9 +33,13 @@ def ConstructGraphFromHemiBrainCSV(LOW_EDGE_THRESHOLD = 5):
         next(csv_reader, None)
         for row in csv_reader:
             neuron_id = int(row[0])
+            type = row[1]
+
             assert (not neuron_id in neurons)
             # add this neuron to the list of neurons
-            neurons.add(neuron_id)
+            neurons.add((neuron_id, type))
+            neuron_ids.add(neuron_id)
+            types.add(type)
 
     # read the synapses by region of interest
     synapse_filename = 'CSVs/HemiBrain/traced-roi-connections.csv'
@@ -46,8 +53,8 @@ def ConstructGraphFromHemiBrainCSV(LOW_EDGE_THRESHOLD = 5):
             post_neuron_id = int(row[1])
             weight = int(row[3])
 
-            assert (pre_neuron_id in neurons)
-            assert (post_neuron_id in neurons)
+            assert (pre_neuron_id in neuron_ids)
+            assert (post_neuron_id in neuron_ids)
 
             # update the connective strength between these two neurons
             if not (pre_neuron_id, post_neuron_id) in edges:
@@ -55,16 +62,60 @@ def ConstructGraphFromHemiBrainCSV(LOW_EDGE_THRESHOLD = 5):
             else:
                 edges[(pre_neuron_id, post_neuron_id)] += weight
 
+    # create a mapping from type names to ints
+    type_mapping = {}
+    for index, type in enumerate(sorted(types)):
+        type_mapping[type] = index
+
     # remove edges that are below a threshold, must first convert keys into a list in python 3
     pruned_edges = [(pre, post) for (pre, post), weight in edges.items() if weight < LOW_EDGE_THRESHOLD]
     for (pre, post) in pruned_edges:
         del edges[(pre, post)]
 
     # construct a directed graph object
+    graph = Graph('hemi-brain-colored', directed = True, colored = True)
+
+    # add vertices with communities initially at -1
+    for enumeration_index, (neuron, type) in enumerate(sorted(neurons)):
+        # vertices start with  the default coloring of -1, enumeration_index of -1
+        graph.AddVertex(neuron, enumeration_index, community = -1, color = type_mapping[type])
+
+    # add edges with the synaptic weights
+    for (pre_neuron_id, post_neuron_id) in edges.keys():
+        graph.AddEdge(pre_neuron_id, post_neuron_id, weight = edges[(pre_neuron_id, post_neuron_id)])
+
+    # create the communities for this graph
+    partitions = graph.DetectCommunities()
+    for vertex, partition in partitions.items():
+        graph.vertices[vertex].community = partition
+
+    # write the hemibrain graph to disk
+    if not os.path.exists('graphs'):
+        os.makedirs('graphs', exist_ok = True)
+    output_filename = 'graphs/hemi-brain-colored.graph.bz2'
+    WriteGraph(graph, output_filename)
+
+    # write the condensed CSV neuron and edge files
+    with open('CSVs/HemiBrain/condensed-neurons.csv', 'w') as fd:
+        fd.write('Neuron ID,Community\n')
+        for neuron in graph.vertices.values():
+            fd.write('{},{},{}\n'.format(neuron.index, neuron.community, neuron.color))
+
+    with open('CSVs/HemiBrain/condensed-edges.csv', 'w') as fd:
+        fd.write('Pre Synaptic Neuron ID,Post Synaptic Neuron ID,Weight\n')
+        for edge in graph.edges:
+            fd.write('{},{},{}\n'.format(edge.source_index, edge.destination_index, edge.weight))
+
+    type_filename = 'CSVs/HemiBrain/type-mapping.pickle'
+    PickleData(type_mapping, type_filename)
+
+    # remove coloring attribute and save as well
+
+    # construct a directed graph object
     graph = Graph('hemi-brain', directed = True, colored = False)
 
     # add vertices with communities initially at -1
-    for enumeration_index, neuron in enumerate(sorted(neurons)):
+    for enumeration_index, (neuron, type) in enumerate(sorted(neurons)):
         # vertices start with  the default coloring of -1, enumeration_index of -1
         graph.AddVertex(neuron, enumeration_index, community = -1, color = -1)
 
@@ -83,16 +134,9 @@ def ConstructGraphFromHemiBrainCSV(LOW_EDGE_THRESHOLD = 5):
     output_filename = 'graphs/hemi-brain.graph.bz2'
     WriteGraph(graph, output_filename)
 
-    # write the condensed CSV neuron and edge files
-    with open('CSVs/HemiBrain/condensed-neurons.csv', 'w') as fd:
-        fd.write('Neuron ID,Community\n')
-        for neuron in graph.vertices.values():
-            fd.write('{},{}\n'.format(neuron.index, neuron.community))
-
-    with open('CSVs/HemiBrain/condensed-edges.csv', 'w') as fd:
-        fd.write('Pre Synaptic Neuron ID,Post Synaptic Neuron ID,Weight\n')
-        for edge in graph.edges:
-            fd.write('{},{},{}\n'.format(edge.source_index, edge.destination_index, edge.weight))
-
     # print statistics
+    print ('No. Neurons: {}'.format(len(neurons)))
+    print ('No. Edges: {}'.format(len(edges)))
+    print ('No. Neuron Types: {}'.format(len(types)))
+
     print ('Wrote {} in {:0.2f} seconds.'.format(output_filename, time.time() - start_time))
