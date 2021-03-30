3ab7aa901ad75e1228de277abf24691e3f58b371
Brian Matejek <bmatejek@gmail.com>
2021-03-30 02:53:06 -0400

    created method for getting ordered list of certificates

---
+++ analysis/certificates.py
@@ -0,0 +1,44 @@
+from addax.utilities.dataIO import ReadGraph
+from addax.kavosh.enumerate import CreateDirectoryStructure
+
+
+
+def ReadCertificates(input_filename, k, vertex_colored, edge_colored, community_based):
+    """
+    Read the certificates for this graph from the subgraphs directory
+
+    @param input_filename: location for the graph to enumerate
+    @parak k: the motif subgraph size to find
+    @param vertex_colored: a boolean flag to allow for vertex colors
+    @param edge_colored: a boolean flag to allow for edge colors
+    @param community_based: a boolean flag to only enumerate subgraphs in the same community
+    """
+    # read the graph
+    graph = ReadGraph(input_filename, vertices_only = True)
+
+    # get the temp directory
+    temp_directory = CreateDirectoryStructure(input_filename, vertex_colored, edge_colored, community_based, False)
+
+    # get the input directory
+    input_directory = 'subgraphs/{}'.format('/'.join(temp_directory.split('/')[1:]))
+
+    # read the combined enumerated subgraphs file
+    subgraphs_filename = '{}/motif-size-{:03d}-certificates.txt'.format(input_directory, k)
+
+    certificates = {}
+
+    # keep track of the total subgraphs
+    total_subgraphs = 0
+
+    with open(subgraphs_filename, 'r') as fd:
+        # read all of the certificates and enumerated subgraphs
+        for subgraph_index, certificate_info in enumerate(fd.readlines()[1:-1]):
+            certificate = certificate_info.split(':')[0].strip()
+            nsubgraphs = int(certificate_info.split(':')[1].strip())
+
+            certificates[certificate] = nsubgraphs
+
+            total_subgraphs += nsubgraphs
+
+    # return the certificates and the number of subgraphs
+    return certificates, total_subgraphs
